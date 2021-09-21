c38ee9f3f865acc756d35bd23b5ba24aac16b86e
gulamrabbanii <43692157+gulamrabbanii@users.noreply.github.com>
2021-09-21 15:04:12 +0530

    Create fibonacci_memoization.c

---
+++ fibonacci_memoization.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int memo[1000];
+
+int fibo(int n){
+  if(memo[n] != -1)
+    return memo[n];
+  
+  if(n == 1)
+    return (memo[n] = 0);
+    
+  if(n == 2)
+    return (memo[n] = 1);
+    
+  return (memo[n] = fibo(n-2) + fibo(n-1));
+  
+}
+int main()
+{
+  int num;
+  scanf("%d", &num);
+  
+  for(int i = 0; i <= num; i++)
+    memo[i] = -1;
+  
+  int res = fibo(num);
+  printf("The term %d in fibonacci is %d", num, res);
+  
+  return 0;
+}
+ 
