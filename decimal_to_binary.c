2bc42fd315019a16cdbf166805ca010d8470afd8
gulamrabbanii <43692157+gulamrabbanii@users.noreply.github.com>
2021-09-23 12:24:54 +0000

    Update decimal_to_binary.c

--- decimal_to_binary.c
+++ decimal_to_binary.c
@@ -1,5 +1,6 @@
 #include <stdio.h> 
 //function to convert decimal number to binary
+//method-01
 int convert(int dec){
     if (dec == 0)
     return 0;
@@ -9,6 +10,18 @@ int convert(int dec){
     }
 }
 
+//method-02
+/*
+void decToBinary(int num)
+{
+    if (num == 0)
+        return;
+
+    decToBinary(num / 2);
+    printf("%d", num % 2);
+}
+*/
+
 int main()
 {
     int dec, bin;
@@ -17,6 +30,8 @@ int main()
     scanf("%d", &dec);
     bin = convert(dec);
     printf("The binary equivalent of %d is %d.\n", dec, bin);
- 
+    
+    //uncomment to test method-02
+    //decToBinary(dec);
     return 0;
 }
